f77f76846f4ffc0920eea0acb9e9d5035b6c5b3d
Ivan Santiago <santylinares70@gmail.com>
2023-09-08 10:39:15 -0300

    First Commit

---
+++ TP4_GRUPO_12/src/package_1/Principal.java
@@ -0,0 +1,12 @@
+package package_1;
+
+public class Principal {
+
+	public static void main(String[] args) 
+	{
+		VentanaPrincipal ventana = new VentanaPrincipal();
+		ventana.ChangeVisibility(true);
+
+	}
+
+}
